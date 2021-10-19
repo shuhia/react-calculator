01ad0dc37c20602b20e1b83518bbc646c9f3c7dd
Alex On <66752902+shuhia@users.noreply.github.com>
2021-10-20 01:47:56 +0200

    refractor button

--- src/scripts/components/button.js
+++ src/scripts/components/button.js
@@ -1,40 +1,27 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import PropTypes from 'prop-types';
 import '../../scss/button.scss';
 
-class Button extends React.Component {
-  constructor() {
-    super();
-
-    this.onClick = this.onClick.bind(this);
-    this.onMouseDown = this.onMouseDown.bind(this);
+function Button(props){
+  const {id, onClick, onMouseDown, className, label} = props;
+  const btn = useRef();
+  const onClick = (evt)=>{
+    evt.preventDefault();
+    evt.currentTarget.blur();
+    onClick(id);
+  }
+  const onMouseDown = (evt)=>{
+      evt.preventDefault();
+      onMouseDown(id);
   }
 
-  shouldComponentUpdate(prevProps) {
-    return prevProps.className !== this.props.className;
-  }
-
-  isActive() {
+  function isActive() {
     const regex = new RegExp(/active/, 'gi');
 
     return this.props.className.match(regex) !== null;
   }
-  onClick(evt) {
-    evt.preventDefault();
-    evt.currentTarget.blur();
 
-    this.props.onClick(this.props.id);
-  }
-  onMouseDown(evt) {
-    evt.preventDefault();
-
-    this.props.onMouseDown(this.props.id);
-  }
-  render() {
-    return (
-      <button ref={(button) => { this.btn = button; }} type={'button'} onTouchStart={this.onMouseDown} onTouchEnd={this.onClick} onMouseDown={this.onMouseDown} onClick={this.onClick} className={this.props.className}>{this.props.label}</button>
-    );
-  }
+  return <button ref={btn} type={'button'} onTouchStart={onMouseDown} onTouchEnd={onClick} onMouseDown={onMouseDown} onClick={onClick} className={className}>{label}</button>
 }
 
 Button.propTypes = {
