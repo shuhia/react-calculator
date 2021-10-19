b9f09949a0e2e7748f88d508adb238e3f795dd4f
Alex On <shuhia.on@gmail.com>
2021-10-20 01:55:21 +0200

    fix button refractor

--- src/scripts/components/button.js
+++ src/scripts/components/button.js
@@ -1,27 +1,32 @@
-import React,{useRef} from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import '../../scss/button.scss';
 
-function Button(props){
-  const {id, onClick, onMouseDown, className, label} = props;
+function Button(props) {
+  const { id, className, label } = props;
   const btn = useRef();
-  const onClick = (evt)=>{
+  const onClick = (evt) => {
     evt.preventDefault();
     evt.currentTarget.blur();
-    onClick(id);
-  }
-  const onMouseDown = (evt)=>{
-      evt.preventDefault();
-      onMouseDown(id);
-  }
+    props.onClick(id);
+  };
+  const onMouseDown = (evt) => {
+    evt.preventDefault();
+    props.onMouseDown(id);
+  };
 
-  function isActive() {
-    const regex = new RegExp(/active/, 'gi');
-
-    return this.props.className.match(regex) !== null;
-  }
-
-  return <button ref={btn} type={'button'} onTouchStart={onMouseDown} onTouchEnd={onClick} onMouseDown={onMouseDown} onClick={onClick} className={className}>{label}</button>
+  return (
+    <button
+      ref={btn}
+      onTouchStart={onMouseDown}
+      onTouchEnd={onClick}
+      onMouseDown={onMouseDown}
+      onClick={onClick}
+      className={className}
+    >
+      {label}
+    </button>
+  );
 }
 
 Button.propTypes = {
@@ -29,7 +34,7 @@ Button.propTypes = {
   label: PropTypes.string,
   onClick: PropTypes.func,
   onMouseDown: PropTypes.func,
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 export default Button;
